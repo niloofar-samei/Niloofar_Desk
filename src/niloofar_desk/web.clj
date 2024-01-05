2e695022c83d71cd64a2f0787e48c8b6d37efaf4
niloofar-samei <niloofar.samei@gmail.com>
2024-01-05 16:19:40 +0000

    Add hiccup to web.clj file.

--- src/niloofar_desk/web.clj
+++ src/niloofar_desk/web.clj
@@ -1,7 +1,20 @@
 (ns niloofar_desk.web
-  (:require [stasis.core :as stasis]))
+  (:require [stasis.core :as stasis]
+            [hiccup.page :refer [html5]]))
 
 (defn get-pages []
   (stasis/slurp-directory "resources/public" #".*\.(html|css|js)$"))
 
 (def app (stasis/serve-pages get-pages))
+
+(defn layout-page [page]
+  (html5
+   [:head
+    [:meta {:charset "utf-8"}]
+    [:meta {:name "viewport"
+            :content "width=device-width, initial-scale=1.0"}]
+    [:title "Tech blog"]
+    [:link {:rel "stylesheet" :href "/styles/styles.css"}]]
+   [:body
+    [:div.logo "cjohansen.no"]
+    [:div.body page]]))
